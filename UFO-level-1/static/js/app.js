fdcca387dc6b0e7e0a73605c66ff877a52a5a39a
Mariano Rivera <marianorivera@Marianos-Air.hsd1.il.comcast.net>
2021-12-06 18:51:07 -0600

    adding code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,48 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+function appendRowsAndData(obj) {
+    var row = tbody.append("tr");
+
+    //loop same order and are present every time
+    Object.entries(obj).forEach(([key, value]) => {
+        row.append("td").text(value); 
+    })
+};
+
+//rows and data
+data.forEach(appendRowsAndData);
+
+//filter
+var button = d3.select("#filter-btn");
+
+//click button
+button.on("click", function() {
+    d3.event.preventDefault();
+
+    //filter date
+    var dateInput = d3.select("#datetime");
+    var datetime = dateInput.property("value");
+
+    var filterInputs = {};
+
+    if (datetime !== "") {
+        filterInputs.datetime = datetime;
+    }
+
+    var filtered = tableData.filter(obj => {
+        var criteria = true;
+        Object.entries(filterInputs).forEach(([key, value]) => {
+            criteria = criteria && (obj[key] === value);
+        });
+        return criteria;
+    });
+
+    console.log(filtered);
+
+    tbody.html("");
+
+    filtered.forEach(appendRowsAndData);
+});
